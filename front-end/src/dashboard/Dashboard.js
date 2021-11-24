a305be162067131ed41418b1bd458d30e75d0c9c
emrahisik <emrahisk@gmail.com>
2021-11-23 22:20:33 -0500

    tables table created

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { listReservations } from "../utils/api";
+import { listReservations, listTables } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
-import { today, next, previous } from "../utils/date-time"
+import { today, next, previous, formatAsTime } from "../utils/date-time"
 
 /**
  * Defines the dashboard page.
@@ -12,7 +12,9 @@ import { today, next, previous } from "../utils/date-time"
 function Dashboard({ date }) {
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
-  const [currentDate, setCurrentDate] = useState(date)
+  const [currentDate, setCurrentDate] = useState(date);
+  const [tables, setTables] = useState([]);
+  const [tablesError, setTablesError] = useState(null);
 
   const clickHandler = ({target}) =>{
     if(target.name === "previous"){
@@ -35,52 +37,120 @@ function Dashboard({ date }) {
       .then(setReservations)
       .catch(setReservationsError);
     return () => abortController.abort();
-  }
+  };
+
+  useEffect(() => {
+    const ac = new AbortController();
+    const loadTables = async () => {
+      try {
+        const tableList = await listTables(ac.signal);
+        setTables(tableList);
+      } catch (error) {
+        setTablesError(error);
+      }
+    };
+    loadTables();
+    return () => ac.abort();
+  }, []);
 
   const reservation = reservations.map((reservation, index) => {
     return (
-      <tr key={index+1}>
+      <tr className="table-secondary" key={reservation.reservation_id}>
         <th scope="row">{index + 1}</th>
-        <td>{reservation.first_name}</td>
-        <td>{reservation.last_name}</td>
+        <td>{reservation.first_name} {reservation.last_name}</td>
         <td>{reservation.mobile_number}</td>
         <td>{reservation.reservation_date}</td>
-        <td>{reservation.reservation_time}</td>
-        <td >{reservation.people}</td>
+        <td>{formatAsTime(reservation.reservation_time)}</td>
+        <td>{reservation.people}</td>
+        <td><a className="btn btn-dark py-0" href={`/reservations/${reservation.reservation_id}/seat`} role="button">Seat</a></td>
       </tr>
     );
 
+});
+
+const table = tables.map((table,index) => {
+  return (
+    <tr className="table-secondary" key={table.table_id}>
+        <th scope="row">{index + 1}</th>
+        <td>{table.table_name} {reservation.last_name}</td>
+        <td>{table.capacity}</td>
+        <td data-table-id-status={table.table_id}>{table.status}</td>
+        <td><button className="btn btn-dark py-0" data-table-id-finish={table.table_id}>Finish</button></td>
+      </tr>
+  )
 })
 
 
-const content = (
-  <table className="table table-striped table-dark">
-    <thead>
+const reservationsTable = (
+  <table className="table table-striped table-light">
+    <thead className="thead-dark">
       <tr>
         <th scope="col">#</th>
-        <th scope="col">First Name</th>
-        <th scope="col">Last Name</th>
-        <th scope="col">Mobile Number</th>
+        <th scope="col">Name</th>
+        <th scope="col">Mobile No</th>
         <th scope="col">Date</th>
         <th scope="col">Time</th>
-        <th scope="col">Number of People</th>
+        <th scope="col">People</th>
+        <th scope="col">Seat</th>
       </tr>
     </thead>
     <tbody>{reservation}</tbody>
   </table>
 );
 
+const tablesTable = (
+  <table className="table table-striped table-light">
+    <thead className="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Table Name</th>
+        <th scope="col">Capacity</th>
+        <th scope="col">Status</th>
+        <th scope="col">Finish</th>
+      </tr>
+    </thead>
+    <tbody>{table}</tbody>
+  </table>
+)
+
   return (
     <main>
       <h1>Dashboard</h1>
-      <button type="button" name="previous" onClick={clickHandler} className="btn btn-dark mr-1"><span className="oi oi-arrow-thick-left"></span> Previous</button>
-      <button type="button" name="today" onClick={clickHandler} className="btn btn-dark">Today</button>
-      <button type="button" name="next" onClick={clickHandler} className="btn btn-dark ml-1">Next <span className="oi oi-arrow-thick-right"></span></button>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+      <button
+        type="button"
+        name="previous"
+        onClick={clickHandler}
+        className="btn btn-dark mr-1"
+      >
+        <span className="oi oi-arrow-thick-left"></span> Previous
+      </button>
+      <button
+        type="button"
+        name="today"
+        onClick={clickHandler}
+        className="btn btn-dark"
+      >
+        Today
+      </button>
+      <button
+        type="button"
+        name="next"
+        onClick={clickHandler}
+        className="btn btn-dark ml-1"
+      >
+        Next <span className="oi oi-arrow-thick-right"></span>
+      </button>
+      <div className="d-md-flex my-3">
+        <h4 className="mb-0">
+          Reservations for {new Date(currentDate + "GMT-5").toDateString()}
+        </h4>
       </div>
       <ErrorAlert error={reservationsError} />
-      {content}
+      <ErrorAlert error={tablesError} />
+      <div className="row">
+        <div className="col-7">{reservationsTable}</div>
+        <div className="col-5">{tablesTable}</div>
+      </div>
     </main>
   );
 }
