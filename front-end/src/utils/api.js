83c31b160b68d5e8030ed2c8e481300a973fa339
emrahisik <emrahisk@gmail.com>
2021-11-28 22:30:26 -0500

    updateTable added

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -102,3 +102,13 @@ export async function createTable(table, signal){
   return await fetchJson(url,options,[]);
 }
 
+export async function updateTable(reservation_id, table_id, signal){
+  const url = `${API_BASE_URL}/tables/${table_id}/seat`;
+  const options = {
+    method: "PUT",
+    headers,
+    body: JSON.stringify({data: { reservation_id: reservation_id }}),
+    signal,
+  };
+  return await fetchJson(url,options,[]);
+}
