0578f1052d9a054c0691465b7fb7997015e1879f
emrahisik <emrahisk@gmail.com>
2021-11-30 14:57:36 -0500

    initial

---
+++ front-end/src/dashboard/ReservationsTable.js
@@ -0,0 +1,80 @@
+import {  updateReservationStatus } from "../utils/api";
+import { formatAsTime } from "../utils/date-time"
+
+
+/**
+ * Defines the dashboard page.
+ * @param date
+ *  the date for which the user wants to view reservations.
+ * @returns {JSX.Element}
+ */
+function ReservationsTable({ reservations, errorHandler }) {
+
+  const statusHandler = async (event, reservation_id) => {
+        try {
+          await updateReservationStatus(reservation_id, "seated");
+        } catch (error) {
+          errorHandler(error);
+        }
+
+  }
+  
+
+  const seatButton = (reservation_id, status) => {
+    return (
+      status === "booked" && (
+        <a
+          className="btn btn-dark py-0"
+          href={`/reservations/${reservation_id}/seat`}
+          role="button"
+          onClick={(event) => statusHandler(event, reservation_id)}
+        >
+          Seat
+        </a>
+      )
+    );
+  };
+
+  const reservation = reservations.map((reservation, index) => {
+    return (
+      <tr className="table-secondary" key={reservation.reservation_id}>
+        <th scope="row">{index + 1}</th>
+        <td>{reservation.first_name} {reservation.last_name}</td>
+        <td>{reservation.mobile_number}</td>
+        <td>{reservation.reservation_date}</td>
+        <td>{formatAsTime(reservation.reservation_time)}</td>
+        <td>{reservation.people}</td>
+        <td data-reservation-id-status={reservation.reservation_id}>{reservation.status}</td>
+        <td>{seatButton(reservation.reservation_id, reservation.status)}</td>
+      </tr>
+    );
+
+});
+
+const reservationsTable = (
+  <table className="table table-striped table-light">
+    <thead className="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Mobile No</th>
+        <th scope="col">Date</th>
+        <th scope="col">Time</th>
+        <th scope="col">People</th>
+        <th scope="col">Status</th>
+        <th scope="col">Seat</th>
+      </tr>
+    </thead>
+    <tbody>{reservation}</tbody>
+  </table>
+);
+
+
+  return (
+    
+        <div className="col-lg-7 ">{reservationsTable}</div>
+        
+  );
+}
+
+export default ReservationsTable;
